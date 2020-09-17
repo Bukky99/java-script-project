cc6bd8acd5d44c3699c3db64979022195d64c434
Bukky <iloveherseys@gmail.com>
2020-09-17 10:21:39 +0100

    commit java project

---
+++ javaScript-project.js
@@ -0,0 +1,55 @@
+//import { myQuestions } from './quizQuestions.js';
+
+
+const storeQuiz = document.getElementById('quiz');
+const storeResults = document.getElementById('results');
+const storeButton = document.getElementById('submit');
+
+function buildQuiz () {
+
+    const output = []; 
+
+     myQuestions.forEach()
+
+   
+
+
+function showResults (){}
+
+//displays quiz
+buildQuiz();
+
+//shows results after submited answer
+submitButton.addEventListener('click', showResults);
+
+const myQuestions = [ 
+    {
+    question: "When is Beyonces birthday?",
+    answers: {
+        a: "May 24th",
+        b: "September 4th",
+        c: "December 23rd",
+    },
+    correctAnswer: "b"
+    },
+    {
+    question: "What language allows you to style a webpage?",
+    answers: {
+        a: "Css",
+        b: "Python",
+        c: "C++"
+    },
+    correctAnswer: "a"
+    },
+    {
+    question: "What is biggest event thats happend in 2020?",
+    answers: {
+        a: "Olympics",
+        b: "World War 3",
+        c: "Lockdown"
+    },
+    correctAnswer: "c"
+}
+]
+
+
---
+++ quizQuestions.js
@@ -0,0 +1,30 @@
+
+ /*const myQuestions = [ 
+    {
+    question: "When is Beyonces birthday?",
+    answers: {
+        a: "May 24th",
+        b: "September 4th",
+        c: "December 23rd",
+    },
+
+    correctAnswer: "b"
+    }
+    {
+    question: "What language allows you to style a webpage?",
+    answers: {
+        a: "Css",
+        b: "Python",
+        c: "C++"
+    },
+    correctAnswer: "a"
+},
+{
+    question: "What is biggest event thats happend in 2020?",
+    answers: {
+        a: "Olympics",
+        b: "World War 3",
+        c: "Lockdown"
+    },
+}
+]*/
