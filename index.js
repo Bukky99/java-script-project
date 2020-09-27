8eba1a203ab18fe89d970fe404cb57e7974dfec8
Bukky <iloveherseys@gmail.com>
2020-09-27 18:35:53 +0100

    Add readme and files

---
+++ index.js
@@ -0,0 +1,126 @@
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+console.log(quizContainer);
+
+function buildQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+    //nested function known as a helper function: a function within a function
+    
+    //function to allow our questions to show up 
+    function showQuestions(questions, quizContainer) {
+
+        //stores chosen answer and correct answers in an empty array
+        let output = [];
+        let answers; //just stores answers as a variable
+
+        // function loops through each question until the end of the questions
+        for(let i = 0; i < questions.length; i++){
+
+            //answers stored in empty array
+            answers = [];
+
+            //'in' operator returns true if answer selected is correct
+            for(letter in questions[i].length){
+
+                //answer then pushed into array. html radio/optional button added 
+                answers.push(
+                    '< label >' // '' tags creates html tags in javascript. JS makes the string and puts in the html (innerhtml)
+                        + '< input type = "radio" name = "question'+ i +'" value = "'+ letter +'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    +'</label>'
+                );
+            }
+
+
+            //adds current question and its chosen answer to output
+            output.push(
+                '< div class = "question">' + questions[i].question + '</div>'
+                + '< div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        //outputs the whole quiz plus the embedded html from js to the html file
+        quizContainer.innerHTML = output.join(' ');
+
+        //This is the end of the showQuestions Funtion
+    };
+    
+
+    showQuestions(questions, quizContainer); // calling the funciton
+
+    function showResults(questions, quizContainer, resultsContainer) {
+
+        //gets the answer container from quiz
+        let answerContainers = quizContainer.querySelectorAll('.answers');
+
+        //stores user's answers
+        let userAnswer = '';
+        let numCorrect = 0;
+
+        for(var i = 0; i < question.length; i++) {
+
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value; // the two line "||" means OR, which means that either a selected answer will be returned OR an empty array/object
+
+            if(userAnswer === questions[i].correctAnswer){
+
+                numCorrect++; //increment this by 1
+                
+
+                answerContainers[i].style.color = 'red'; //both quiz and answer will go red
+            }
+
+            else{
+
+                answerContainers[i].style.color = 'red'; //if answer not correct or left blank
+            };
+        };
+
+        resultsContainer.innerHTML = numCorrect + 'out of ' + questions.length;
+
+        //This is the end of the showResults Function
+    };
+
+    //when user clicks button, answers will show
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    };
+
+    // this is the end of the buildQuiz function
+};
+
+    const myQuestions = [ 
+        {
+            question: "When is Beyonces birthday?",
+            answers: {
+                a: 'May 24th',
+                b: 'September 4th',
+                c: 'December 23rd',
+            },
+            correctAnswer: 'b'
+        },
+        {
+            question: "What programming language allows you to style a webpage?",
+            answers: {
+                a: 'Css',
+                b: 'Python',
+                c: 'C++'
+            },
+            correctAnswer: 'a' 
+        },
+        {
+            question: "What is biggest event thats happend in 2020?",
+            answers: {
+                a: 'Olympics',
+                b: 'World War 3',
+                c: 'Lockdown'
+            },
+            correctAnswer: 'c'
+        }
+    ];
+
+
+ buildQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
--- javaScript-project.js
+++ javaScript-project.js
@@ -1,55 +1,128 @@
 //import { myQuestions } from './quizQuestions.js';
 
+/*
+let storeQuiz = document.getElementById('quiz');
+let storeResults = document.getElementById('results');
+let submitButton = document.getElementById('submit');
 
-const storeQuiz = document.getElementById('quiz');
-const storeResults = document.getElementById('results');
-const storeButton = document.getElementById('submit');
+function buildQuiz (questions, storeQuiz, storeResults, submitButton) {
 
-function buildQuiz () {
+    function showQuestions(questions, storeQuiz){
 
-    const output = []; 
+        //stores the output of the answer choice
+        let output = [];
+        let results;
 
-     myQuestions.forEach()
+        //for each question up until the max number of questions
+        for(let i=0; i<questions.length; i++){
 
-   
+            restuls = [];
+
+            for(letter in questions[i].answers){
+              
+                //radio button is an optional button
+                ansers.push(
+                    '<label'
+                    + '<input type="radio" name="question'+i+'"value="'+letter+'">'
+                    + letter + ': '
+                    + questions[i].answers[letter]
+                    + '</label'
+                );
+
+            }
+
+            //adds the current question and its associated answer to the output 
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+    }
+
+    storeQuiz.innerHTML = output.join('');
+
+    function showResults (questions, storeQuiz, storeResults){
+
+        //gets answers from quiz
+        let storeResults = storeQuiz.querySelectorAll('.answers');
+
+        // tracks users answers
+        let userAnswer = '';
+        //number of correct answers
+        let numCorrect = 0
+
+        // for each question
+        for (let i=0; i<questions.length; i++){
+
+             //finds the selected answer
+            userAnswer = (storeResults[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            //if answer is correct
+            if(userAnswer===questions[i].correctAnswer) {
+
+                //adds to the number of correct answers
+                numCorrect++;
+
+                //correct answer goes green
+                storeQuiz[i].style.color = 'lightgreen';
+            }
+
+            //if answer is black or incorrect
+            else{
+                // colors it red
+                storeQuiz[i].style.color = 'red';
+            }
+
+        }
+
+        storeResults.innerHTML = numCorrect + '= out of ' + questions.length;
+    }
 
 
-function showResults (){}
 
-//displays quiz
-buildQuiz();
+    //shows the question in the quiz container
+    showQuestions(questions, storeQuiz);
 
-//shows results after submited answer
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [ 
-    {
-    question: "When is Beyonces birthday?",
-    answers: {
-        a: "May 24th",
-        b: "September 4th",
-        c: "December 23rd",
-    },
-    correctAnswer: "b"
-    },
-    {
-    question: "What language allows you to style a webpage?",
-    answers: {
-        a: "Css",
-        b: "Python",
-        c: "C++"
-    },
-    correctAnswer: "a"
-    },
-    {
-    question: "What is biggest event thats happend in 2020?",
-    answers: {
-        a: "Olympics",
-        b: "World War 3",
-        c: "Lockdown"
-    },
-    correctAnswer: "c"
+    // when user clicks submit button, reuslts show
+    submitButton.onlick = function(){
+        showResults(questions, storeQuiz, storeResults);
+    }
 }
+     
+
+let myQuestions = [ 
+    {
+    question: 'When is Beyonces birthday?',
+    answers: {
+        a: 'May 24th',
+        b: 'September 4th',
+        c: 'December 23rd',
+    },
+    correctAnswer: 'b'
+    },
+    {
+    question: 'What programming language allows you to style a webpage?',
+    answers: {
+        a: 'Css',
+        b: 'Python',
+        c: 'C++'
+    },
+    correctAnswer: 'a' 
+    },
+    {
+    question: 'What is biggest event thats happend in 2020?',
+    answers: {
+        a: 'Olympics',
+        b: 'World War 3',
+        c: 'Lockdown'
+    },
+    correctAnswer: 'c'
+}
+
 ]
 
+showQuestions(questions, storeQuiz);
 
+buildQuiz(myQuestions, storeQuiz, storeResults, submitButton);
+}
+
+*/
